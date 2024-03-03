8abf7b862394bb43ed3859f8c14b4f3ec6eb6983
Hyper-Dev1 <sudipzdaahaal@gmail.com>
2024-03-03 22:06:37 +0545

    minor home changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,98 @@
-// import { useState } from 'react'
-
+import "./scss/app.scss"
 
 function App() {
-
   return (
     <>
-      <div>
-        <h1>hello</h1>
-      </div>
+      <nav>
+        <div className="nav-logo">
+          <h1>Home Furniture</h1>
+          {/* <img src="./vite.svg" alt="" /> */}
+        </div>
+        <div className="nav-link">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Catalog</a>
+            </li>
+            <li>
+              <a href="/">All Products</a>
+            </li>
+            <li>
+              <a href="/">New Arrival</a>
+            </li>
+            <li>
+              <a href="/">Trending</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-buttons">
+          <button>Profile</button>
+          <button>Search</button>
+          <button>Wishlist</button>
+          <button>Cart</button>
+        </div>
+      </nav>
+      <main>
+        <div className="hero">
+          <div className="hero-img"></div>
+          <div className="hero-text">
+            <h2>hello</h2>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div className="newsletter">
+          <div className="newsletter-info">
+            <h3>Newsletter</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ut
+              enim non, nemo optio, perspiciatis sit ipsa qui doloremque,
+              quibusdam asperiores provident iusto nisi delectus quis in eaque!
+              Eligendi, placeat.
+            </p>
+          </div>
+          <form action="/">
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+            />
+            <input
+              type="mail"
+              name="email"
+              placeholder="Enter your email address"
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <div className="footer-links">
+          <p>&copy; Copyright Home Furniture 2023</p>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Catalog</a>
+            </li>
+            <li>
+              <a href="/">All Products</a>
+            </li>
+            <li>
+              <a href="/">New Arrival</a>
+            </li>
+            <li>
+              <a href="/">Trending</a>
+            </li>
+          </ul>
+          <div className="footer-socials">
+            <button>Facebook</button>
+            <button>Facebook</button>
+            <button>Facebook</button>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
