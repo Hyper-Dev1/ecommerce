bc4e7bb8b4d9629efec1e2e277ada15f3a3a7480
Hyper-Dev1 <sudipzdaahaal@gmail.com>
2024-03-16 17:57:18 +0545

    landingpage-comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,117 @@
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom"
 import Homepage from "./Components/Homepage"
-// import Productpage from "./Components/Productpage"
+import Productpage from "./Components/Productpage"
 // import Shipping from "./Components/Shipping"
-// import Cart from "./Components/Cart"
-// import Category from "./Components/Category"
+import Cart from "./Components/Cart"
+import Category from "./Components/Category"
 import "./scss/app.scss"
 
 function App() {
   return (
     <>
-      <nav>
-        <div className="nav-logo">
-          <h1>Home Furniture</h1>
-        </div>
-        <div className="nav-link">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Catalog</a>
-            </li>
-            <li>
-              <a href="/">All Products</a>
-            </li>
-            <li>
-              <a href="/">New Arrival</a>
-            </li>
-            <li>
-              <a href="/">Trending</a>
-            </li>
-          </ul>
-        </div>
-        <div className="nav-buttons">
-          <button>Profile</button>
-          <button>Search</button>
-          <button>Wishlist</button>
-          <button>Cart</button>
-        </div>
-      </nav>
-      <main>
-        <Homepage />
-        {/* <Productpage /> */}
-        {/* <Shipping /> */}
-        {/* <Cart /> */}
-        {/* <Category /> */}
-      </main>
-      <footer>
-        {/* <div className="newsletter">
-          <div className="newsletter-info">
-            <h3>Newsletter</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ut
-              enim non, nemo optio, perspiciatis sit ipsa qui doloremque,
-              quibusdam asperiores provident iusto nisi delectus quis in eaque!
-              Eligendi, placeat.
-            </p>
+      <Router>
+        <nav>
+          <div className="nav-logo">
+            <h1>Home Furniture</h1>
           </div>
-          <form action="/">
-            <input type="text" name="name" placeholder="Enter your name" />
-            <input
-              type="mail"
-              name="email"
-              placeholder="Enter your email address"
-            />
-            <button type="submit">Submit</button>
-          </form>
-        </div> */}
-        <div className="footer-links">
-          <p>&copy; Copyright Home Furniture 2023</p>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Catalog</a>
-            </li>
-            <li>
-              <a href="/">All Products</a>
-            </li>
-            <li>
-              <a href="/">New Arrival</a>
-            </li>
-            <li>
-              <a href="/">Trending</a>
-            </li>
-          </ul>
-          <div className="footer-socials">
-            <button>Facebook</button>
-            <button>Facebook</button>
-            <button>Facebook</button>
+          <div className="nav-link">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/shop/category">Catalog</Link>
+              </li>
+              <li>
+                <Link to="/shop/product/all">All Products</Link>
+              </li>
+              <li>
+                <a href="/">New Arrival</a>
+              </li>
+              <li>
+                <a href="/">Trending</a>
+              </li>
+            </ul>
           </div>
-        </div>
-      </footer>
+          <div className="nav-buttons">
+            <button>
+              <Link to="/shop/cart">
+                <img src="/Assets/bag.svg" alt="Cart" />
+              </Link>
+            </button>
+            <button>
+              <Link to="/">
+                <img src="/Assets/profilec.svg" alt="Profile" />
+              </Link>
+            </button>
+            <button>
+              <Link to="/">
+                <img src="/Assets/search-normal-1.svg" alt="Search" />
+              </Link>
+            </button>
+            <button>
+              <Link to="/shop/wishlist">
+                <img src="/Assets/heart.svg" alt="Wishlist" />
+              </Link>
+            </button>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" exact Component={Homepage} />
+          <Route path="/shop/category" Component={Category} />
+          <Route path="/shop/product/all" Component={Category} />
+          <Route path="/shop/cart" Component={Cart} />
+          <Route path="/shop/wishlist" Component={Cart} />
+          <Route path="/shop/product/id=23" Component={Productpage} />
+          <Route path="/shop/product/sale" Component={Category} />
+          <Route path="/shop/category/categoryname" Component={Category} />
+        </Routes>
+        <footer>
+          <hr />
+          <div className="newsletter">
+            <div className="newsletter-info">
+              <h3>Newsletter</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ut
+                enim non, nemo optio, perspiciatis sit ipsa qui doloremque,
+                quibusdam asperiores provident iusto nisi delectus quis in
+                eaque! Eligendi, placeat.
+              </p>
+            </div>
+            <form action="/">
+              <input type="text" name="name" placeholder="Enter your name" />
+              <input
+                type="mail"
+                name="email"
+                placeholder="Enter your email address"
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+          <div className="footer-links">
+            <p>&copy; Copyright Home Furniture 2023</p>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/shop/product">Catalog</Link>
+              </li>
+              <li>
+                <Link to="/shop/product/all">All Products</Link>
+              </li>
+              <li>
+                <a href="/">New Arrival</a>
+              </li>
+              <li>
+                <a href="/">Trending</a>
+              </li>
+            </ul>
+            <div className="footer-socials">
+            </div>
+          </div>
+        </footer>
+      </Router>
     </>
   )
 }
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,22 +1,27 @@
+import { Link } from "react-router-dom"
 const Homepage = () => {
   return (
-    <>
+    <main>
       <div className="hero">
-        <div className="hero-text">
-          <h2>Comfortable Sofa with Great Comfort</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-            consequuntur adipisci in, similique deleniti est reiciendis ducimus
-            illum ipsum? Blanditiis tenetur illum, corporis iusto qui porro
-            tempora non vero voluptates!
-          </p>
-          <button className="cta">Shop Now</button>
-        </div>
-        <div className="hero-img">
-          <img
-            src="https://pluspng.com/img-png/furniture-png-chair-png-transparent-image-1096.png"
-            alt=""
-          />
+        <div className="corousel-item">
+          <div className="hero-text">
+            <h2>Comfortable Sofa with Great Comfort</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+              consequuntur adipisci in, similique deleniti est reiciendis
+              ducimus illum ipsum? Blanditiis tenetur illum, corporis iusto qui
+              porro tempora non vero voluptates!
+            </p>
+            <button className="cta">
+              <Link to="/shop/product/id=23">Shop Now</Link>
+            </button>
+          </div>
+          <div className="hero-img">
+            <img
+              src="https://pluspng.com/img-png/furniture-png-chair-png-transparent-image-1096.png"
+              alt=""
+            />
+          </div>
         </div>
       </div>
       <div className="sale-items-cont">
@@ -28,10 +33,12 @@ const Homepage = () => {
           </h2>
           <hr />
           <p>Our entire October collection</p>
-          <button className="cta">Explore</button>
+          <button className="cta">
+            <Link to="/shop/product/sale">Explore</Link>
+          </button>
         </div>
         <div className="sale-items">
-          <a href="/" className="card">
+          <Link to="/shop/product/id=23" className="card">
             <div className="card-cont">
               <div className="card-img">
                 <img
@@ -44,8 +51,8 @@ const Homepage = () => {
                 <p>Rs 00000</p>
               </div>
             </div>
-          </a>
-          <a href="/" className="card">
+          </Link>
+          <Link to="/shop/product/id=23" className="card">
             <div className="card-cont">
               <div className="card-img">
                 <img
@@ -58,8 +65,8 @@ const Homepage = () => {
                 <p>Rs 00000</p>
               </div>
             </div>
-          </a>
-          <a href="/" className="card">
+          </Link>
+          <Link to="/shop/product/id=23" className="card">
             <div className="card-cont">
               <div className="card-img">
                 <img
@@ -72,8 +79,8 @@ const Homepage = () => {
                 <p>Rs 00000</p>
               </div>
             </div>
-          </a>
-          <a href="/" className="card">
+          </Link>
+          <Link to="/shop/product/id=23" className="card">
             <div className="card-cont">
               <div className="card-img">
                 <img
@@ -86,20 +93,53 @@ const Homepage = () => {
                 <p>Rs 00000</p>
               </div>
             </div>
-          </a>
+          </Link>
         </div>
       </div>
-      <div className="popular-grid-cont">
-        <div className="sofa"></div>
-        <div className="bed"></div>
-        <div className="lamp"></div>
-        <div className="armchair"></div>
-        <div className="chair"></div>
-        <div className="textile"></div>
-        <div className="decor"></div>
-        <div className="mirror"></div>
+      <div className="popular">
+        <h2>Popular</h2>
+        <div className="popular-grid-cont">
+          <Link to="/shop/category/categoryname" className="sofa">
+            <img src="/Assets/category/sofa.jpg" alt="image" />
+            <h3>Sofa</h3>
+          </Link>
+          <Link to="/shop/category/categoryname" className="item-height bed">
+            <img src="/Assets/category/bedroom.jpg" alt="image" />
+            <h3>Bedroom</h3>
+          </Link>
+          <Link to="/shop/category/categoryname" className="item-height lamp">
+            <img src="/Assets/category/lamp.jpg" alt="image" />
+            <h3>Lamp</h3>
+          </Link>
+          <Link
+            to="/shop/category/categoryname"
+            className="item-height armchair"
+          >
+            <img src="/Assets/category/armchair.jpg" alt="image" />
+            <h3>Armchair</h3>
+          </Link>
+          <Link to="/shop/category/categoryname" className="chair">
+            <img src="/Assets/category/chair.jpg" alt="image" />
+            <h3>Chair</h3>
+          </Link>
+          <Link
+            to="/shop/category/categoryname"
+            className="item-height textile"
+          >
+            <img src="/Assets/category/textile.jpg" alt="image" />
+            <h3>Textile</h3>
+          </Link>
+          <Link to="/shop/category/categoryname" className="item-height decor">
+            <img src="/Assets/category/decor.jpg" alt="image" />
+            <h3>Decoration</h3>
+          </Link>
+          <Link to="/shop/category/categoryname" className="item-height mirror">
+            <img src="/Assets/category/mirror.jpg" alt="image" />
+            <h3>Mirror</h3>
+          </Link>
+        </div>
       </div>
-    </>
+    </main>
   )
 }
 
